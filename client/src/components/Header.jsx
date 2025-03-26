3d12dc9b07942a9bdfed567d9d92f38463b71372
Amar <amarjha880@gmail.com>
2025-03-26 15:45:24 +0530

    first commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div >
+      <div>
+        <div>
+          <img src="" alt="logo" />
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>corporate partnership</li>
+            <select name="explore universities" id="">
+              <option value="university Catalog">university Catalog</option>
+              <option value="Course Catalog">Course Catalog</option>
+            </select>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <button>WhatsApp Us</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
