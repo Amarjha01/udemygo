48171dfe67fda5614957be4a3b1ec9359331daef
Amar <amarjha880@gmail.com>
2025-04-02 17:22:29 +0530

    updated config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,19 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   
   plugins: [
     tailwindcss(),
     react()],
-    base:'/udemygo'
+    base:'/',
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
 })
