b0b797aeea785cb36e3ce9bff2768b611abbb55d
abhishekdav003 <abhishekdav003@gmail.com>
2025-04-02 09:22:50 +0530

    change

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from "../components/About/Hero"
+import Founder from '../components/About/Founder'
 
-const About = () => {
+function About() {
   return (
-    <div>About</div>
+    <>
+      <Hero />
+      <Founder />
+    </>
   )
 }
 
